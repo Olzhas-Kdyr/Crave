3f038ca3e5bf35d953b72037305994c1cdd7d04f
Kdyr Olzhas <kdiroljas@gmail.com>
2026-05-01 13:02:59 +0500

    Refactor build script for new repository structure
    Updated repository URLs and branches for device and vendor trees, adjusted kernel and hardware cloning commands, and modified build commands.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # repo init
-repo init -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr2 --git-lfs
+repo init -u https://github.com/Lineage-Olzhas/manifest.git -b lineage-23.2 --git-lfs --depth 1
 
 # repo sync script
 /opt/crave/resync.sh
@@ -11,41 +11,40 @@ kernel/xiaomi
 vendor/xiaomi
 hardware/xiaomi
 vendor/lineage-priv/keys
-vendor/clover-priv/keys
 out/target/product/munch
 )
 
 rm -rf "${remove[@]}"
 
 # Deivce Trees
-git clone --depth=1 https://github.com/olzhas0986/android_device_xiaomi_munch -b clover-16 device/xiaomi/munch
-git clone --depth=1 https://github.com/olzhas0986/android_device_xiaomi_sm8250-common.git -b clover-16-qpr2 device/xiaomi/sm8250-common
+git clone https://github.com/olzhas0986/android_device_xiaomi_munch device/xiaomi/munch
 
 # Vendor Trees
 git clone --depth=1 https://github.com/olzhas0986/android_vendor_xiaomi_munch vendor/xiaomi/munch
-git clone --depth=1 https://github.com/olzhas0986/android_vendor_xiaomi_sm8250-common vendor/xiaomi/sm8250-common
 
 # Munch Firmware
 git clone --depth=1 https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware
 
 # Kernel Tree
-git clone --depth=1 https://github.com/olzhas0986/magictime_sm8250_ximi -b aosp-16 kernel/xiaomi/sm8250
+git clone --depth=1 https://github.com/olzhas0986/kernel_xiaomi_munch -b staging kernel/xiaomi/munch
 
 # Hardware Xiaomi
-git clone --depth=1 https://github.com/olzhas0986/android_hardware_xiaomi -b aosp-16 hardware/xiaomi
+git clone https://github.com/crdroidandroid/android_hardware_xiaomi -b 16.0 hardware/xiaomi
 
 # MIUI Camera
 git clone --depth=1 https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
 
 # My Keys
-git clone --depth=1 https://github.com/olzhas0986/keys -b cl vendor/lineage-priv/keys
+git clone https://github.com/olzhas0986/keys vendor/lineage-priv/keys
 
-# Soong
-git clone --depth=1 https://github.com/olzhas0986/build_soong-clover -b 16-qpr2 build/soong
+cd kernel/xiaomi/munch
+git submodule update --init
+rm -rf KernelSU-Next/userspace/su
+cd ../../..
 
 # Building 
 . build/envsetup.sh
 export SKIP_ABI_CHECKS=true
 make installclean
-lunch clover_munch-bp4a-user
-mka clover
+lunch lineage_munch-bp4a-user
+m bacon

c1fb6a3be3d527d7d58571da8c05c541c0a94015
k.olzhas <kdiroljas@gmail.com>
2026-07-24 15:47:21 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 # repo init
-rm -rf .repo/manifests
-rm -rf .repo/local_manifests
-rm -rf .repo/manifest
-repo init -u https://github.com/Lineage-Olzhas/manifest.git -b lineage-23.2 --git-lfs --depth 1
+repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs --depth 1
 
 # repo sync script
 /opt/crave/resync.sh
@@ -21,29 +18,24 @@ packages/apps/Launcher3
 rm -rf "${remove[@]}"
 
 # Deivce Trees
-git clone https://github.com/olzhas0986/android_device_xiaomi_munch device/xiaomi/munch
+git clone https://github.com/olzhas0986/android_device_xiaomi_munch -b 16.2-Rebased device/xiaomi/munch
 
 # Vendor Trees
-git clone --depth=1 https://github.com/olzhas0986/android_vendor_xiaomi_munch vendor/xiaomi/munch
+git clone --depth=1 https://github.com/olzhas0986/android_vendor_xiaomi_munch -b 16.2-Rebased vendor/xiaomi/munch
 
 # Munch Firmware
 git clone --depth=1 https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware.git vendor/xiaomi/munch-firmware
 
 # Kernel Tree
-git clone --depth=1 https://github.com/olzhas0986/kernel_xiaomi_munch -b staging kernel/xiaomi/munch
+git clone --depth=1 https://github.com/olzhas0986/android_kernel_xiaomi_sm8250 -b new kernel/xiaomi/sm8250
 
 # Hardware Xiaomi
-git clone https://github.com/crdroidandroid/android_hardware_xiaomi -b 16.0 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 # MIUI Camera
 git clone --depth=1 https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git vendor/xiaomi/miuicamera
 
-# My Keys
-git clone https://github.com/olzhas0986/keys vendor/lineage-priv/keys
-
-git clone https://github.com/olzhas0986/android_packages_apps_Launcher3 packages/apps/Launcher3
-
-cd kernel/xiaomi/munch
+cd kernel/xiaomi/sm8250
 git submodule update --init
 rm -rf KernelSU-Next/userspace/su
 cd ../../..
